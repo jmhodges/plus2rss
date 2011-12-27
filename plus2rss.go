f6adf2e0ff33edeaf02a355a745da9c6abc5bbc8
Jeff Hodges <jeff@somethingsimilar.com>
2011-12-26 18:28:06 -0800

    adding flags for http socket timeout

--- plus2rss.go
+++ plus2rss.go
@@ -9,11 +9,6 @@ import (
 	"time"
 )
 
-const (
-	frontendReadTimeoutNanos  = time.Duration(4e8) // 400 millis
-	frontendWriteTimeoutNanos = time.Duration(4e8) // 400 millis
-)
-
 type Service interface {
 	ShutdownChan() chan string
 }
@@ -22,30 +17,34 @@ type Service interface {
 func main() {
 	var frontendHost string
 	var frontendAddr string
-	var clientSecret string
+	var simpleKey string
+	var frontendReadTimeout = time.Duration(4e8) // 400 millis
+	var frontendWriteTimeout = time.Duration(4e8) // 400 millis
 
 	flag.StringVar(&frontendHost, "vhost", "localhost:6543", "the virtual Host header to respond to in the frontend")
 	flag.StringVar(&frontendAddr, "http", "localhost:6543", "address to run the frontend o (e.g. :6543, localhost:4321)")
-	flag.StringVar(&clientSecret, "csecret", "", "OAuth2 consumer secret for Google")
+	flag.StringVar(&simpleKey, "simpleKey", "", "Simple API Access key for Google")
+	flag.DurationVar(&frontendReadTimeout, "frontendReadTimeout", frontendReadTimeout, "frontend http server's socket read timeout")
+	flag.DurationVar(&frontendWriteTimeout, "frontendWriteTimeout", frontendWriteTimeout, "frontend http server's socket write timeout")
 	flag.Parse()
 
-	fs, err := feedStorage(clientSecret)
+	fs, err := feedStorage(simpleKey)
 	if err != nil {
 		log.Fatalf("Could not boot feed storage: %s", err)
 	}
 	FeedStore = fs
 
-	f, server := frontend(frontendHost, frontendAddr, frontendReadTimeoutNanos, frontendWriteTimeoutNanos)
+	f, server := frontend(frontendHost, frontendAddr, frontendReadTimeout, frontendWriteTimeout)
 	_ = server
 	err = <-f.ShutdownChan()
 	log.Printf("frontend shutdown: %s", err)
 }
 
-func feedStorage(clientSecret string) (FeedStorage, error) {
-	if strings.Trim(clientSecret, " \r\n\t") == "" {
+func feedStorage(simpleKey string) (FeedStorage, error) {
+	if strings.Trim(simpleKey, " \r\n\t") == "" {
 		return nil, errors.New("Google API client secret cannot be blank")
 	}
-	retriever := &FeedRetriever{http.DefaultClient, clientSecret}
+	retriever := &FeedRetriever{http.DefaultClient, simpleKey}
 	return retriever, nil
 }
 
