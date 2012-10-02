57a613d1799d71ce1d2425367b2d993a4984c9d6
Jeff Hodges <jeff@somethingsimilar.com>
2012-10-02 15:11:38 -0700

    clean up flag handling and use TrimSpace

--- plus2rss.go
+++ plus2rss.go
@@ -13,35 +13,31 @@ type Service interface {
 	ShutdownChan() chan string
 }
 
+const socketTimeout = time.Duration(400 * time.Millisecond)
+
+var (
+	frontendHost         = flag.String("vhost", "localhost:6543", "the virtual Host header to respond to in the frontend")
+	frontendAddr         = flag.String("http", "localhost:6543", "address to run the frontend on (e.g. :6543, localhost:4321)")
+	simpleKeyFile        = flag.String("simpleKeyFile", "", "File containing simple API access key for Google")
+	templateDir          = flag.String("templateDir", "./templates", "Directory containing the templates to render html and feeds")
+	frontendReadTimeout  = flag.Duration("frontendReadTimeout", socketTimeout, "frontend http server's socket read timeout")
+	frontendWriteTimeout = flag.Duration("frontendWriteTimeout", socketTimeout, "frontend http server's socket write timeout")
+)
+
 // TODO: fix timestamps, add attachments, handle posts that were reshares
 func main() {
-	var frontendHost string
-	var frontendAddr string
-	var simpleKeyFile string
-	var templateDir string
-	var frontendReadTimeout = time.Duration(400 * time.Millisecond)
-	var frontendWriteTimeout = time.Duration(400 * time.Millisecond)
-
-	flag.StringVar(&frontendHost, "vhost", "localhost:6543", "the virtual Host header to respond to in the frontend")
-	flag.StringVar(&frontendAddr, "http", "localhost:6543", "address to run the frontend o (e.g. :6543, localhost:4321)")
-	flag.StringVar(&simpleKeyFile, "simpleKeyFile", "", "File containing simple API access key for Google")
-	flag.StringVar(&templateDir, "templateDir", ".", "Directory containing the templates to render html and feeds")
-
-	flag.DurationVar(&frontendReadTimeout, "frontendReadTimeout", frontendReadTimeout, "frontend http server's socket read timeout")
-	flag.DurationVar(&frontendWriteTimeout, "frontendWriteTimeout", frontendWriteTimeout, "frontend http server's socket write timeout")
 	flag.Parse()
 
-	if simpleKeyFile == "" {
+	if *simpleKeyFile == "" {
 		log.Fatalf("plus2rss: -simpleKeyFile=FILE is a required command-line argument")
 	}
 
-	fs, err := feedStorage(simpleKeyFile)
+	fs, err := feedStorage(*simpleKeyFile)
 	if err != nil {
 		log.Fatalf("Could not boot feed storage: %s", err)
 	}
 
-	f, server := frontend(fs, frontendHost, frontendAddr, templateDir, frontendReadTimeout, frontendWriteTimeout)
-	_ = server
+	f := frontend(fs, *frontendHost, *frontendAddr, *templateDir, *frontendReadTimeout, *frontendWriteTimeout)
 	err = <-f.ShutdownChan()
 	log.Printf("frontend shutdown: %s", err)
 }
@@ -51,12 +47,12 @@ func feedStorage(simpleKeyFile string) (FeedStorage, error) {
 	if err != nil {
 		return nil, err
 	}
-	simpleKey := strings.Trim(string(simpleKeySlice), " \r\n\t")
+	simpleKey := strings.TrimSpace(string(simpleKeySlice))
 	retriever := &FeedRetriever{http.DefaultClient, simpleKey}
 	return retriever, nil
 }
 
-func frontend(fs FeedStorage, host, addr, templateDir string, readTimeout, writeTimeout time.Duration) (*Frontend, *http.Server) {
+func frontend(fs FeedStorage, host, addr, templateDir string, readTimeout, writeTimeout time.Duration) *Frontend {
 	f := NewFrontend(fs, host, templateDir)
 	server := &http.Server{addr, f, readTimeout, writeTimeout, 0, nil}
 
@@ -64,5 +60,5 @@ func frontend(fs FeedStorage, host, addr, templateDir string, readTimeout, write
 		err := server.ListenAndServe()
 		f.ShutdownChan() <- err
 	}()
-	return f, server
+	return f
 }
