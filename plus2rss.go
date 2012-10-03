6c2babfc37593ad8c1d9c824389dca45c250096a
Jeff Hodges <jeff@somethingsimilar.com>
2012-10-02 20:27:14 -0700

    ditch OAuth2 but keep using the client

--- plus2rss.go
+++ plus2rss.go
@@ -1,9 +1,7 @@
 package main
 
 import (
-	"code.google.com/p/goauth2/oauth"
 	"code.google.com/p/google-api-go-client/plus/v1"
-	"errors"
 	"flag"
 	"io/ioutil"
 	"log"
@@ -21,8 +19,7 @@ const socketTimeout = time.Duration(400 * time.Millisecond)
 var (
 	frontendHost         = flag.String("vhost", "localhost:6543", "the virtual Host header to respond to in the frontend")
 	frontendAddr         = flag.String("http", "localhost:6543", "address to run the frontend on (e.g. :6543, localhost:4321)")
-	oauthSecretFile      = flag.String("oauthSecretFile", "", "file containing a working Google OAuth2 client id and secret")
-	oauthCacheFile       = flag.String("oauthCacheFile", "", "file containing a cached Google OAuth2 access and refresh token")
+	simpleKeyFile      = flag.String("simpleKeyFile", "", "file containing a working Google simple key")
 	templateDir          = flag.String("templateDir", "./templates", "Directory containing the templates to render html and feeds")
 	frontendReadTimeout  = flag.Duration("frontendReadTimeout", socketTimeout, "frontend http server's socket read timeout")
 	frontendWriteTimeout = flag.Duration("frontendWriteTimeout", socketTimeout, "frontend http server's socket write timeout")
@@ -32,15 +29,11 @@ var (
 func main() {
 	flag.Parse()
 
-	if *oauthSecretFile == "" {
-		log.Fatalf("plus2rss: -oauthSecretFile=FILE is a required command-line argument")
+	if *simpleKeyFile == "" {
+		log.Fatalf("plus2rss: -simpleKeyFile=FILE is a required command-line argument")
 	}
 
-	if *oauthCacheFile == "" {
-		log.Fatalf("plus2rss: -oauthCacheFile=FILE is a required command-line argument")
-	}
-
-	fs, err := feedStorage(*oauthSecretFile, *oauthCacheFile)
+	fs, err := feedStorage(*simpleKeyFile)
 	if err != nil {
 		log.Fatalf("Could not boot feed storage: %s", err)
 	}
@@ -50,40 +43,14 @@ func main() {
 	log.Printf("frontend shutdown: %s", err)
 }
 
-func feedStorage(secretFile, cacheFile string) (FeedStorage, error) {
-	oauthSecret, err := ioutil.ReadFile(secretFile)
+func feedStorage(simpleFile string) (FeedStorage, error) {
+	simpleKey, err := ioutil.ReadFile(simpleFile)
 	if err != nil {
 		return nil, err
 	}
-	lines := strings.Split(string(oauthSecret), "\n")
-	if len(lines) < 3 {
-		return nil, errors.New("not enough lines in oauth config file")
-	}
-
-	clientId := strings.TrimSpace(lines[0])
-	secret := strings.TrimSpace(lines[1])
-
-	c := &oauth.Config{
-		ClientId:     clientId,
-		ClientSecret: secret,
-		Scope:        plus.PlusMeScope,
-		AuthURL:      "https://accounts.google.com/o/oauth2/auth",
-		TokenURL:     "https://accounts.google.com/o/oauth2/token",
-	}
-
-	transport := &oauth.Transport{
-		Config:    c,
-		Transport: http.DefaultTransport,
-	}
-	tokenCache := oauth.CacheFile(cacheFile)
-	tok, err := tokenCache.Token()
-	if err != nil {
-		return nil, err
-	}
-
-	// Handle refreshing tokens outside of this process
-	transport.Token = &oauth.Token{AccessToken: tok.AccessToken}
-	srv, err := plus.New(transport.Client())
+	key := strings.TrimSpace(string(simpleKey)) // FIXME make json
+	t := &SimpleKeyTransport{Key: key, Transport: http.DefaultTransport}
+	srv, err := plus.New(&http.Client{Transport: t})
 	if err != nil {
 		return nil, err
 	}
---
+++ plus_api.go
@@ -0,0 +1,17 @@
+package main
+
+import (
+	"net/http"
+)
+
+type SimpleKeyTransport struct {
+	Key string
+	Transport http.RoundTripper
+}
+
+func (t *SimpleKeyTransport) RoundTrip(r *http.Request) (*http.Response, error) {
+	q := r.URL.Query()
+	q.Set("key", t.Key)
+	r.URL.RawQuery = q.Encode()
+	return t.Transport.RoundTrip(r)
+}
